eb6f1b63ca9a3874cf73f2bf33c6da521435d798
Khuni X Villan <130685566+KhuniVillan@users.noreply.github.com>
2023-05-26 14:17:59 +0530

    Update start.py

--- Bikash/utils/inline/start.py
+++ Bikash/utils/inline/start.py
@@ -28,7 +28,7 @@ def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         ],
         [           
             InlineKeyboardButton(
-                text="📱 𝐘𝐨𝐮𝐭𝐮𝐛𝐞 📱", url=f"https://youtube.com/@BikashGadgetsTech"
+                text="📱 𝐎𝐰𝐧𝐞𝐫 📱", url=f"https://t.me/@MrKhunii"
             )
         ],
      ]
@@ -58,7 +58,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         ],
         [           
             InlineKeyboardButton(
-                text="📱 𝐘𝐨𝐮𝐭𝐮𝐛𝐞 📱", url=f"https://youtube.com/@BikashGadgetsTech"
+                text="📱 𝐎𝐰𝐧𝐞𝐫 📱", url=f"https://t.me/@MrKhunii"
             )
         ],
         [
